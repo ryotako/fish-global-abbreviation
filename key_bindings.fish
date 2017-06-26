41e65376f397114e969502ce7c19dcfb9bf948d3
Kimura Masayuki <masa0x80@gmail.com>
2017-06-26 23:59:03 +0900

    Add config file path variable
    Default config file path is`$HOME/.config/fish/.gabbr.conf`
    or `$XDG_CONFIG_HOME/fish/.gabbr.conf`.

--- key_bindings.fish
+++ key_bindings.fish
@@ -1,1 +1,6 @@
+if not set -q gabbr_config
+    set -U gabbr_config "$HOME/.config/fish/.gabbr.conf"
+    set -q XDG_CONFIG_HOME
+    and set gabbr_config "$XDG_CONFIG_HOME/fish/.gabbr.conf"
+end
 bind ' ' '__gabbr_expand; commandline -i " "'
