ba4d381ce80d0090b9d78dfc1340316f42068f28
Ryota Kobayashi <rytakov@gmail.com>
2017-07-06 21:52:42 +0900

    Add tests
    test of --show, --list, --reload

--- test/gabbr.fish
+++ test/gabbr.fish
@@ -3,17 +3,57 @@ function setup
 end
 
 test "add an abbreviation"
-    "G" = (gabbr --add G '| grep'
-        gabbr --list)
+    "G" = (
+        gabbr --add G '| grep'
+        gabbr --list
+        )
 end
 
 test "count abbreviations"
-    1 = (count (gabbr --add G '| grep'
-        gabbr --list))
+    1 = (count (
+        gabbr --add G '| grep'
+        gabbr --list
+        ))
 end
 
 test "erase an abbreviation"
-    0 = (count (gabbr --add G '| grep'
+    0 = (count (
+        gabbr --add G '| grep'
         gabbr --erase G
-        gabbr --list))
+        gabbr --list
+        ))
+end
+
+test "show abbreviations"
+    "gabbr G '| grep'" "gabbr L '| less'" = (
+        gabbr --add G '| grep'
+        gabbr --add L '| less'
+        gabbr --show
+        )
+end
+
+test "list abbreviations"
+    G L = (
+        gabbr --add G '| grep'
+        gabbr --add L '| less'
+        gabbr --list
+        )
+end
+
+
+test "add function abbreviations"
+    "gabbr E -f 'echo expanded'" = (
+        gabbr --function E 'echo expanded'
+        gabbr --show
+        )
+end
+
+
+test "reload abbereviations"
+    G = (set -g gabbr_config (dirname (realpath $FILENAME))"/.gabbr.config"
+        echo "G | grep" > "$gabbr_config"
+        gabbr --reload
+        gabbr --list
+        rm "$gabbr_config"
+        )
 end
