0562cd9cf429ae707537cdc00465b6ad69a7fdb0
questbeat <questbeat@gmail.com>
2019-01-01 01:16:35 +0900

    Expand user abbreviations without using $fish_user_abbreviations

--- functions/__gabbr_expand.fish
+++ functions/__gabbr_expand.fish
@@ -6,10 +6,11 @@ function __gabbr_expand
 
     # expand abbreviations
     if test 0 = (count (commandline -poc))
-        for abbr in $fish_user_abbreviations
-            echo $abbr | read word phrase
+        for word in (abbr -l)
             if test "$word" = (commandline -t)
-                commandline -t $phrase
+                set escaped_word (string escape --style=var -- $word)
+                set phrase "_fish_abbr_$escaped_word"
+                commandline -t $$phrase
             end
         end
     end
